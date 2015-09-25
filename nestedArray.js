16b530df3db52a38e7023094f94339cfee67cbca
awwakum <awwakum@gmail.com>
2015-09-25 10:25:43 +0200

    first commit

---
+++ nestedArray.js
@@ -0,0 +1,20 @@
+//Flatten a nested array. You must account for varying levels of nesting.
+function steamroller(arr) {
+    
+    function isArr(elem) {
+        return Array.isArray(elem);
+    }
+    
+    do {
+            arr = [].concat.apply([], arr);
+    } while (arr.some(isArr)); 
+        
+        
+    console.log(arr);
+    return arr;
+}
+
+steamroller([1, [2], [3, [[4]]]]);
+steamroller([[['a']], [['b']]]);
+steamroller([1, [], [3, [[4]]]]);
+steamroller([1, {}, [3, [[4]]]]);
